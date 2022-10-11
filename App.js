6e1fa056c8d6f831f13d90561196a47c66afaabe
Tanusree24 <tanusree241996@gmail.com>
2022-10-11 10:49:20 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, ScrollView, Text, View} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch('https://covid-19-statistics.p.rapidapi.com/regions', {
+      method: 'GET',
+      url: 'https://covid-19-statistics.p.rapidapi.com/regions',
+      headers: {
+        'X-RapidAPI-Key': '5ad52ae891msha57664fb78d9e38p1be7edjsn79ae240f9e3a',
+        'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com',
+      },
+    })
+      .then(reponse => reponse.json())
+      .then(data => {
+        console.log('data', data);
+        setIsLoading(false);
+        setResponse(data);
+      })
+      .catch(error => {
+        console.log('something failed miserably');
+        setIsLoading(false);
+        setError(true);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {isLoading ? (
+        <ActivityIndicator size={30} />
+      ) : error ? (
+        <Text>ERROR OCCURED</Text>
+      ) : (
+        <ScrollView>
+          <Text>{JSON.stringify(response)}</Text>
+        </ScrollView>
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
